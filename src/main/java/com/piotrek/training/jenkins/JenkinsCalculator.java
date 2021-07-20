7c23e122aa9465dc82ad2ecfeacc8fc985ca09cf
robalrop <robalrop@gmail.com>
2021-07-20 19:11:18 +0200

    Adding new files

---
+++ src/main/java/com/piotrek/training/jenkins/JenkinsCalculator.java
@@ -0,0 +1,17 @@
+package com.piotrek.training.jenkins;
+
+public class JenkinsCalculator {
+
+	public int addNumbers(int numberOne, int numberTwo) {
+//		int var1 = 20;
+//		System.out.println("This is a sample message");
+		return numberOne + numberTwo;
+		}
+		
+	public int subtractNumbers(int numberOne, int numberTwo) {
+		return numberOne - numberTwo;
+		}
+	
+	
+	
+}
